e980fdf068b60a791337d63c6736cede4e80e18c
justinyeh1995 <justinyeh1995@gmail.com>
2024-03-31 18:58:20 -0400

    Initial Commit

---
+++ CybORG-ARViz/CybORG_ARVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CybORG_ARVizApp.swift
+//  CybORG-ARViz
+//
+//  Created by Justin Yeh on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CybORG_ARVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CybORG-ARVizUITests/CybORG_ARVizUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CybORG_ARVizUITestsLaunchTests.swift
+//  CybORG-ARVizUITests
+//
+//  Created by Justin Yeh on 3/31/24.
+//
+
+import XCTest
+
+final class CybORG_ARVizUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
