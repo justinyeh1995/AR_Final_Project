784db8ebc547d184c0bd57241080342d2ad9e07c
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-01 15:45:12 -0400

    AR View Container

---
+++ CybORG-ARViz/ARViewContainer.swift
@@ -0,0 +1,23 @@
+//
+//  ARViewContainer.swift
+//  CybORG-ARViz
+//
+//  Created by Justin Yeh on 4/1/24.
+//
+
+import SwiftUI
+import ARKit
+import RealityKit
+
+struct ARViewContainer: UIViewRepresentable {
+    typealias UIViewType = ARView
+    
+    func makeUIView(context: Context) -> ARView {
+        <#code#>
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        <#code#>
+    }
+}
+
