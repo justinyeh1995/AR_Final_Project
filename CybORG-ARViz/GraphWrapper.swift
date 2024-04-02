434923523f1ac983d4267deec2d3803b872c2fb1
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-01 21:07:59 -0400

    graph data structure

--- CybORG-ARViz/GraphWrapper.swift
+++ CybORG-ARViz/GraphWrapper.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+//
+//  GraphWrapper.swift
+//  CybORG-ARViz
+//
+//  Created by Justin Yeh on 4/1/24.
+//
+
+import Foundation
+
+
+struct NetworkGraph: Codable {
+    let nodes: [Node]
+    let links: [Link]
+}
+
+struct Node: Codable {
+    let id: String
+}
+
+struct Link: Codable {
+    let source: String
+    let target: String
+}
+
+// Assuming "Blue" and "Red" have similar structure, create a wrapper
+struct GraphWrapper: Codable {
+    let Blue: GraphDetails
+    let Red: GraphDetails
+}
+
+struct GraphDetails: Codable {
+    let link_diagram: NetworkGraph
+    // Add other properties as needed
+}
