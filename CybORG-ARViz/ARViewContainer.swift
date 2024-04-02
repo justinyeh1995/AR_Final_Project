360a19afde3943787a75520caa7e3d9ff2248013
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-01 21:07:24 -0400

    add building graph view

--- CybORG-ARViz/ARViewContainer.swift
+++ CybORG-ARViz/ARViewContainer.swift
@@ -12,12 +12,34 @@ import RealityKit
 struct ARViewContainer: UIViewRepresentable {
     typealias UIViewType = ARView
     
+    var graphData: GraphWrapper? // Assume this is your graph data model
+
+    
     func makeUIView(context: Context) -> ARView {
-        <#code#>
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
-        <#code#>
+        // Use graphData to update your AR scene
+        if let graphData = graphData {
+            // Clear existing anchors if any
+            uiView.scene.anchors.removeAll()
+            
+            // Setup the AR content with the new graph data
+//            setupGraphInView(arView: uiView, graphData: graphData)
+        }
+    }
+    
+    private func setupGraphInView(arView: ARView, graphData: GraphWrapper) {
+        // Here you would convert your graphData to 3D models and place them in the AR scene.
+        // This is a placeholder for where you would implement the visualization based on your graphData.
+        
+        // Example: Create an anchor and add entities to it
+//        let anchor = AnchorEntity(plane: .horizontal)
+//        // Add nodes and links as entities to the anchor
+//        // ...
+//        arView.scene.addAnchor(anchor)
     }
 }
 
---
+++ CybORG-ARViz/GraphWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  GraphWrapper.swift
+//  CybORG-ARViz
+//
+//  Created by Justin Yeh on 4/1/24.
+//
+
+import Foundation
