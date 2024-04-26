d0b1c7d202925e2d7597b9e41dd248a7ae1484fb
justinyeh1995 <justinyeh1995@gmail.com>
2024-04-26 17:20:07 -0500

    add tab later

--- CybORG-ARViz/ARViewCoordinator.swift
+++ CybORG-ARViz/ARViewCoordinator.swift
@@ -5,6 +5,8 @@
 //  Created by Justin Yeh on 4/21/24.
 //
 
+// Ref: https://gist.github.com/azamsharp/7f2c5957cab1ac0c7daf97bef1a7da26
+
 import RealityKit
 import UIKit ///use for handling screen Actions
 
@@ -24,6 +26,7 @@ class ARViewCoordinator: NSObject {
         let tapLocation = recognizer.location(in: recognizer.view)
         
         if let entity = view.entity(at: tapLocation) as? ModelEntity, !entity.name.isEmpty {
+            
         }
         
     }
